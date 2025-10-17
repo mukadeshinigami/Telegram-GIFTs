551775f5be349bba5083287ae8631d45b5eeba38
mukadeshinigami <mukade.official@gmail.com>
2025-10-18 01:06:08 +0200

    Parse fragment - startt

---
+++ app/parser/fragment.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+}
+
+def parse (
+    
+):
+    None
+
+
+if __name__ == "__main__":
+    None
