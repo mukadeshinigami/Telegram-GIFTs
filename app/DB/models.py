b7a75a8103bc54026f30617b5299a7f4f136f8ae
mukadeshinigami <mukade.official@gmail.com>
2025-10-21 14:22:15 +0200

    Added SQLAlchemy re create_database.py add f mmodels.py

---
+++ app/DB/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Gift(Base):
+    __tablename__ = 'gifts'
+    
+    num = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer)
+    name = Column(String)
+    model = Column(String)
+    backdrop = Column(String)
+    symbol = Column(String)
+    sale_price = Column(String)
+    
+    rarity_score = Column(Float)
+    estimated_price = Column(Float)
+    date_added = Column(DateTime)
